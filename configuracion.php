85eb598353f9fb812dccd649e860d995be8f980c
javierGaliana <javier.galiana.romero@gmail.com>
2013-10-21 05:15:58 +0200

    Practica 1

---
+++ configuracion.php
@@ -0,0 +1,6 @@
+<?php
+  define("DB_DSN", "mysql:host=".$_SERVER['DB1_HOST'].";dbname=".$_SERVER['DB1_NAME'].";port=".$_SERVER['DB1_PORT']); 
+  define("DB_USUARIO", $_SERVER['DB1_USER'] ); 
+  define("DB_CONTRASENIA", $_SERVER['DB1_PASS'] ); 
+?>
+
